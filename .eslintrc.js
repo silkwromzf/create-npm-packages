f5245d2a01e1ba43a2b9a3dde8ea0b30c4152421
silk <silkwormcoc@gmail.com>
2022-04-10 21:35:39 +0800

    add configs

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  globals: {
+    NodeJS: true,
+  },
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+     // 在导出的函数和类的公共类方法上需要显式的返回值和参数类型
+    '@typescript-eslint/explicit-module-boundary-types': ['off'],
+    
+    'prettier/prettier': [
+      'error',
+      {
+        'endOfLine': 'auto',
+        "semi": true,
+        "singleQuote": true,
+        "printWidth": 120,
+        "tabWidth": 4,
+      }
+    ],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
